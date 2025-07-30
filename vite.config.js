eb3eff30d7dad339d5c8f227e16a44816bd05469
laifugroup <laifuygj@gmail.com>
2025-07-30 11:22:32 +0800

    v1.0.0

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
-    base: "/",
+    base: process.env.NODE_ENV === 'production' ? '/vite-goal-prod/' : '/',
     plugins: [react(), tailwindcss()],
     resolve: {
         alias: {
